bc0ce3f05364afd10627cbadb162bdabef07eb8b
nikmosi <i@nikmosi.ru>
2024-10-19 21:50:05 +0700

    feat(config): implement Settings class

---
+++ src/ai_telegram_bot/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="aibot_", env_file=".env", env_file_encoding="utf-8"
+    )
+
+    token: str = "xxx"
