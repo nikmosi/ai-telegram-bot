0939c152b23c1e2f150eb9b6691d6b8c10892e16
nikmosi <i@nikmosi.ru>
2024-10-23 10:45:57 +0700

    refactor: extract gpt to class

--- src/ai_telegram_bot/TelegramBotAI.py
+++ src/ai_telegram_bot/TelegramBotAI.py
@@ -1,26 +1,40 @@
-from aiogram.types import Message
-
 from collections import defaultdict
-import g4f
+from collections.abc import AsyncGenerator, Generator
+from contextlib import asynccontextmanager, contextmanager
+from tempfile import NamedTemporaryFile
+from typing import Any
+
+import ffmpeg
+import speech_recognition as sr
 from aiogram import F, Router
 from aiogram.filters import Command
-from g4f import Provider
-import speech_recognition as sr
-import ffmpeg
+from aiogram.types import Message
+from gpt import Gpt, GptArgs
+
 from ai_telegram_bot.config import Settings
 
 main_route = Router()
-conversation_history = defaultdict(list)
 settings = Settings()
+gpts = defaultdict(
+    lambda: Gpt(
+        GptArgs(
+            model=settings.model,
+            provider=settings.provider,
+            proxy=settings.proxy,
+            api_key=settings.api_key,
+        )
+    )
+)
 
 
-def convert_audio(input_path: str, output_format: str = "wav") -> str:
-    output_path = f"voice_converted.{output_format}"
-
-    # Используем ffmpeg для конвертации
-    ffmpeg.input(input_path).output(output_path).run(overwrite_output=True)
-
-    return output_path
+@contextmanager
+def convert_audio_format(
+    input_path: str, output_format: str = "wav"
+) -> Generator[str, Any]:
+    with NamedTemporaryFile(suffix=output_format) as tmp_file:
+        output_path = tmp_file.name
+        ffmpeg.input(input_path).output(output_path).run(overwrite_output=False)
+        yield output_path
 
 
 def recognize_audio(file_path: str) -> dict:
@@ -46,6 +60,27 @@ def get_user_id(message: Message) -> int:
     return user.id
 
 
+def get_user_text(message: Message) -> str:
+    text = message.text
+    if text is None:
+        raise Exception("text is None")
+    return text
+
+
+def get_file_id(message: Message):
+    voice = message.voice
+    if voice is None:
+        raise Exception("voice is None")
+    return voice.file_id
+
+
+def get_bot(message: Message):
+    bot = message.bot
+    if bot is None:
+        raise Exception("bot is None")
+    return bot
+
+
 def trim_history(history, max_length=4096):
     current_length = sum(len(message["content"]) for message in history)
     while history and current_length > max_length:
@@ -57,81 +92,54 @@ def trim_history(history, max_length=4096):
 @main_route.message(Command("clear"))
 async def process_clear_command(message: Message):
     user_id = get_user_id(message)
-    conversation_history[user_id] = []
+    gpts.pop(user_id)
     await message.answer("История диалога очищена.")
 
 
 @main_route.message(F.text)
 async def handle_message(message: Message):
     user_id = get_user_id(message)
-    user_input = message.text
-
-    conversation_history[user_id].append({"role": "user", "content": user_input})
-    conversation_history[user_id] = trim_history(conversation_history[user_id])
-
-    chat_history = conversation_history[user_id]
-    using_provider = settings.provider
+    user_text = get_user_text(message)
+    gpt = gpts[user_id]
 
     try:
-        chat_gpt_response = await g4f.ChatCompletion.create_async(
-            messages=chat_history,
-            model=settings.model,
-            provider=using_provider,
-            proxy=settings.proxy,
-            api_key=settings.api_key,
-        )
+        response = await gpt.ask(user_text)
     except Exception:
-        chat_gpt_response = "Извините, произошла ошибка."
+        response = "Извините, произошла ошибка."
+    await message.answer(response)
 
-    conversation_history[user_id].append(
-        {"role": "assistant", "content": chat_gpt_response}
-    )
-    await message.answer(chat_gpt_response)
+
+@asynccontextmanager
+async def download_voice(message: Message) -> AsyncGenerator[str, Any]:
+    file_id = get_file_id(message)
+    bot = get_bot(message)
+    file = await bot.get_file(file_id)
+    if file.file_path is None:
+        raise Exception("file_path is None")
+    with NamedTemporaryFile(suffix=".ogg") as tmp_file:
+        temp_path = tmp_file.name
+        await bot.download_file(file.file_path, temp_path)
+        yield temp_path
+
+
+def convert_voice_to_text(voice: str) -> str:
+    with convert_audio_format(voice) as converted_file:
+        result = recognize_audio(converted_file)
+
+        if result.get("error"):
+            raise Exception(result.get("error"))
+        return result["text"]
 
 
 @main_route.message(F.voice)
 async def handle_voice_message(message: Message):
-    user = message.from_user
-    if not user:
-        return
-    user_id = user.id
+    user_id = get_user_id(message)
+    gpt = gpts[user_id]
+    async with download_voice(message) as voice:
+        text = convert_voice_to_text(voice)
 
-    voice = message.voice
-    file_id = voice.file_id
-    file = await message.bot.get_file(file_id)
-    await message.bot.download_file(file.file_path, "voice.ogg")
-
-    # Конвертируем аудио из OGG в WAV
-    converted_file = convert_audio("voice.ogg")
-
-    # Распознаем текст из аудио
-    result = recognize_audio(converted_file)
-
-    if result.get("error"):
-        await message.answer(result["error"])
-        return
-
-    user_input = result["text"]
-
-    # Добавляем распознанный текст в историю общения
-
-    conversation_history[user_id].append({"role": "user", "content": user_input})
-    conversation_history[user_id] = trim_history(conversation_history[user_id])
-
-    chat_history = conversation_history[user_id]
-    using_provider = Provider.ChatGptEs
-
-    try:
-        chat_gpt_response = await g4f.ChatCompletion.create_async(
-            model="gpt-4o",  # Учтите, что модель должна быть корректна
-            messages=chat_history,
-            provider=using_provider,
-            proxy=settings.proxy,
-        )
-    except Exception:
-        chat_gpt_response = "Извините, произошла ошибка."
-
-    conversation_history[user_id].append(
-        {"role": "assistant", "content": chat_gpt_response}
-    )
-    await message.answer(chat_gpt_response)
+        try:
+            response = await gpt.ask(text)
+        except Exception:
+            response = "Извините, произошла ошибка."
+        await message.answer(response)
