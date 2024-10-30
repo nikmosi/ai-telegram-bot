7177f5b6457613943a864f4686ad8d1e83d1b905
nikmosi <i@nikmosi.ru>
2024-10-30 21:55:14 +0700

    feat(logging): add logging middleware

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from loguru import logger
 
 from ai_telegram_bot.data.config import Settings
 from ai_telegram_bot.handlers.user import prepare_router as prepare_user_router
+from ai_telegram_bot.middlewares.logging import LoggingMiddleware
 from ai_telegram_bot.utils import gpt_provider
 from ai_telegram_bot.utils.logging import setup_logger
 
@@ -23,6 +25,10 @@ async def setup_handlers(dispatcher: Dispatcher) -> None:
     dispatcher.include_router(prepare_user_router())
 
 
+def setup_middlewares(dispatcher: Dispatcher) -> None:
+    dispatcher.update.outer_middleware(LoggingMiddleware())
+
+
 async def setup_aiogram(dispatcher: Dispatcher) -> None:
     setup_logging(dispatcher)
     setup_gpt_provider(dispatcher)
@@ -30,6 +36,7 @@ async def setup_aiogram(dispatcher: Dispatcher) -> None:
 
     logger.debug("Configuring aiogram")
     await setup_handlers(dispatcher)
+    setup_middlewares(dispatcher)
     logger.info("Configured aiogram")
 
 
@@ -46,6 +53,13 @@ async def aiogram_on_shutdown_polling(dispatcher: Dispatcher, bot: Bot) -> None:
 async def main():
     dp = Dispatcher()
     bot = Bot(token=settings.token, default=DefaultBotProperties(parse_mode="HTML"))
+    logger.add(
+        "logs/telegram_bot.log",
+        level="DEBUG",
+        format="{time} | {level} | {module}:{function}:{line} | {message}",
+        rotation="100 KB",
+        compression="zip",
+    )
     dp.startup.register(aiogram_on_startup_polling)
     dp.shutdown.register(aiogram_on_shutdown_polling)
     await dp.start_polling(bot)
