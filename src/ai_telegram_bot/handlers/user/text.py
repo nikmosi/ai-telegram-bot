23aebbc0a3f1448ef3a12f89393e99aedfeedf1f
nikmosi <i@nikmosi.ru>
2024-11-08 19:12:55 +0700

    chore(handlers/text): make parallel requests to gpt

--- src/ai_telegram_bot/handlers/user/text.py
+++ src/ai_telegram_bot/handlers/user/text.py
@@ -1,7 +1,9 @@
+from asyncio import TaskGroup
+
 from aiogram.types import Message
 
 from ai_telegram_bot.handlers.user.taro import play
-from ai_telegram_bot.services.gpt import Gpt, answer_on_text
+from ai_telegram_bot.services.gpt import answer_on_text, determine_taro_request
 from ai_telegram_bot.utils.gpt_provider import GptProvider
 
 
@@ -12,20 +14,19 @@ async def handle_message(message: Message, gpt_provider: GptProvider) -> None:
     user_id = message.from_user.id
     text = message.text
     gpt = gpt_provider.get(user_id)
+    taro = gpt_provider.get_taro_gpt(user_id)
 
-    is_taro_request = await determine_taro_request(text, gpt)
+    pending = []
+
+    async with TaskGroup() as tg:
+        pending.append(tg.create_task(determine_taro_request(text, taro)))
+        pending.append(tg.create_task(answer_on_text(text, gpt)))
+
+    pending = [await task for task in pending]
+
+    is_taro_request, response = pending
 
     if is_taro_request:
         await play(message)
     else:
-        response = await answer_on_text(text, gpt)
         await message.reply(response)
-
-
-async def determine_taro_request(text: str, gpt: Gpt) -> bool:
-    prompt = (
-        f"Определи, является ли запрос '{text}' запросом к таро. Ответь 'Да' или 'Нет'."
-    )
-    response = await answer_on_text(prompt, gpt)
-
-    return "Да" in response.strip()
