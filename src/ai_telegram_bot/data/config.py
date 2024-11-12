f96a8f975a6ad9774b5917aa174933f1df7e6d15
nikmosi <i@nikmosi.ru>
2024-11-12 13:13:52 +0700

    chore: add naming_convention

--- src/ai_telegram_bot/data/config.py
+++ src/ai_telegram_bot/data/config.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from g4f import Provider, ProviderType
 from g4f.Provider import ProviderUtils
-from pydantic import BaseModel, Field, field_validator, PostgresDsn
+from pydantic import BaseModel, Field, PostgresDsn, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -12,6 +12,14 @@ class DatabaseConfig(BaseModel):
     echo_pool: bool = False
     max_overflow: int = 50
     pool_size: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
--- src/ai_telegram_bot/models/base.py
+++ src/ai_telegram_bot/models/base.py
@@ -1,9 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+from ai_telegram_bot.data import settings
 from ai_telegram_bot.utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
