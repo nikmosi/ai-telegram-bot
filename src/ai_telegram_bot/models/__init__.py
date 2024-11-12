a1b9200087cdae975c3cfa734d19e74efc8fb6c7
nikmosi <i@nikmosi.ru>
2024-11-12 12:54:57 +0700

    chore(__init__): add classes

--- src/ai_telegram_bot/models/__init__.py
+++ src/ai_telegram_bot/models/__init__.py
@@ -1,4 +1,5 @@
 from .gpt import GptArgs
 from .user import UserInfo
+from .base import Base
 
-__all__ = ["UserInfo", "GptArgs"]
+__all__ = ["UserInfo", "GptArgs", "Base"]
--- src/ai_telegram_bot/utils/__init__.py
+++ src/ai_telegram_bot/utils/__init__.py
@@ -1,3 +1,4 @@
 from .voice import convert_voice_to_text
+from .case_converter import camel_case_to_snake_case
 
-__all__ = ["convert_voice_to_text"]
+__all__ = ["convert_voice_to_text", "camel_case_to_snake_case"]
