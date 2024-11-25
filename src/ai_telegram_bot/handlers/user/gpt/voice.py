f7b62bb0539162c1461906f8bb92a14dac0324b7
nikmosi <i@nikmosi.ru>
2024-11-25 13:20:41 +0700

    chore(handlers): extract common function

--- src/ai_telegram_bot/handlers/user/gpt/voice.py
+++ src/ai_telegram_bot/handlers/user/gpt/voice.py
@@ -1,16 +1,11 @@
 from aiogram.types import Message
 
-from ai_telegram_bot.utils import convert_voice_to_text
+from ai_telegram_bot.handlers.shared import extract_text_from_voice
 from ai_telegram_bot.utils.gpt_provider import GptProvider
 
 from .text import handle_message
 
 
 async def handle_voice_message(message: Message, gpt_provider: GptProvider) -> None:
-    if message.from_user is None:
-        return
-    answer = await message.reply("Распознаю...")
-    recognition_text = await convert_voice_to_text(message)
-    message_with_text = message.model_copy(update={"text": recognition_text})
-    await answer.edit_text(f"{recognition_text=}")
+    message_with_text = await extract_text_from_voice(message)
     await handle_message(message_with_text, gpt_provider)
--- src/ai_telegram_bot/handlers/user/taro/additional_info.py
+++ src/ai_telegram_bot/handlers/user/taro/additional_info.py
@@ -2,8 +2,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from redis.asyncio import Redis
 
+from ai_telegram_bot.handlers.shared.voice import extract_text_from_voice
 from ai_telegram_bot.states import user
-from ai_telegram_bot.utils import convert_voice_to_text
 from ai_telegram_bot.utils.gpt_provider import GptProvider
 
 from .taro import cmd_taro_end
@@ -24,10 +24,5 @@ async def handle_message(
 async def handle_voice_message(
     message: Message, redis: Redis, state: FSMContext, gpt_provider: GptProvider
 ) -> None:
-    if message.from_user is None:
-        return
-    answer = await message.reply("Распознаю...")
-    recognition_text = await convert_voice_to_text(message)
-    message_with_text = message.model_copy(update={"text": recognition_text})
-    await answer.edit_text(f"{recognition_text=}")
+    message_with_text = await extract_text_from_voice(message)
     await handle_message(message_with_text, redis, state, gpt_provider)
